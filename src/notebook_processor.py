3831b2fb7e144bf7b9275b1c442f2b6ff8b781c1
Daisuke Fujiwara <daisuke.fujiwara@gmail.com>
2025-06-07 23:24:10 -0700

    Add notebook result processor to save to DB (#5)

---
+++ src/notebook_processor.py
@@ -0,0 +1,10 @@
+from src.repositories.book_repository import BookRepository
+from src.repositories.note_repository import NoteRepository
+from src.notebook_parser import NotebookParseResult
+
+
+def process_notebook_result(result: NotebookParseResult, book_repo: BookRepository, note_repo: NoteRepository):
+    # Example logic to process the result
+    # This is where you would interact with the repositories
+    # For example, you might want to add books or notes to the database
+    pass
